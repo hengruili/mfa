3b6806b20a3634139f651f8351bbed3acdd0f72a
Henry Li <hengruili@gmail.com>
2023-01-07 21:08:38 +1100

    first commit

---
+++ packages/container/src/bootstrap.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//mount function to start the app
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,23 @@
+import React, {useRef, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import MaterialLink from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+import {mount} from 'marketing/MarketingApp';
+
+
+export default () => {
+  const ref = useRef(null);
+  useEffect(()=>{
+    mount(ref.current);
+  });
+
+  return <div ref={ref}></div>
+}
