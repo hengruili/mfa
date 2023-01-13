e877fcf15b69c957408529623ab4e2d73da1bf77
Henry Li <hengruili@gmail.com>
2023-01-14 08:50:43 +1100

    trigger workflow

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 //mount function to start the app
-
 const mount = (el) => {
     ReactDOM.render(
         <App/>,
